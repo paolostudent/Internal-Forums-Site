15d870795c2c94e96e00e0317ebb6deb12660104
paolostudent <19115@my.sanctamaria.school.nz>
2024-04-03 10:05:49 +1300

    Homepage route routes to base.html file. (Trial 2)

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint     
+from flask import Blueprint, render_template  
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 def home():
-    return "<h1>Header</h1><p>This is a paragraph</p>"
+    return render_template("base.html")
